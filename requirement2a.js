37471779e71b939208af26371c07b85a145e24ec
Gabriel Caetano <gabrielvscaetano@gmail.com>
2024-08-23 18:36:12 -0300

    clean code smells on requirement 2a

--- requirement2a.js
+++ requirement2a.js
@@ -2,19 +2,16 @@ const Storage = require('./services/storage');
 
 async function requirement2a(repository) {
   try {
-    console.log(process.env.DOC_NAME);
     const api_data = await Storage.findByName(process.env.DOC_NAME, repository);
-    console.log({ api_data });
 
-    const records = api_data.doc_record
-    console.log({ records });
+    const records = api_data.doc_record;
     const filtered = records.filter((r) => {
       const year = parseInt(r.Year);
       return [2020, 2019, 2018].includes(year);
-    })
-    console.log({ filtered });
+    });
     const sum = filtered.reduce((acc, r) => { return acc + parseInt(r.Population) }, 0);
-    console.log({ sum });
+    console.log('result2a >> sum of population', sum);
+
   } catch (e) {
     console.log(e);
   }
