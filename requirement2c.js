836a1d4e4740bf36f4ed707c0a6d5633d0828ff0
Gabriel Caetano <gabrielvscaetano@gmail.com>
2024-08-23 18:37:08 -0300

    requirement 2c

---
+++ requirement2c.js
@@ -0,0 +1,16 @@
+// receive the database connection
+// query for the sum of the population of 2018, 2019 and 2020 and log the result
+async function requirement2c(repository) {
+  try {
+    const res = await repository.population_sum.find({ doc_name: process.env.DOC_NAME });
+    console.log(res);
+
+    const { sum } = res[0];
+    console.log('result2c >> sum of population', sum);
+
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = requirement2c;
